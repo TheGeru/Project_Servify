afaf16686805f0878520c88d7d85ead68faa9d75
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2025-11-22 18:18:49 -0600

    Agregado notificaciones y perfil

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+
+class NotificationService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+  Future<void> initNotifications() async {
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      String? token = await _messaging.getToken();
+      if (kDebugMode) {
+        print('FCM Token: $token');
+      }
+    }
+  }
+
+  /// Creamos una notificación en la colección 'notifications' de Firestore (Notificación In-App
+  /// si gustan editarlo)
+  Future<void> sendNotification({
+    required String toUserId, // ID del proveedor que recibe
+    required String fromUserId, // ID del cliente que envía
+    required String fromUserName,
+    required String title,
+    required String body,
+    required String type,
+  }) async {
+    await _db.collection('notifications').add({
+      'toUserId': toUserId,
+      'fromUserId': fromUserId,
+      'fromUserName': fromUserName,
+      'title': title,
+      'body': body,
+      'type': type,
+      'read': false,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
+
+  /// Aqui mero se obtiene el stream de notificaciones para el usuario actual
+  Stream<QuerySnapshot> getUserNotifications(String userId) {
+    return _db
+        .collection('notifications')
+        .where('toUserId', isEqualTo: userId)
+        .orderBy('createdAt', descending: true)
+        .snapshots();
+  }
+}
