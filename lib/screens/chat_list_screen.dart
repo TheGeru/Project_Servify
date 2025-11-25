6587db887141d949c99ce3dcfa1c6362b8920e59
Danieluvh <137945972+Daniel18UvH@users.noreply.github.com>
2025-11-25 15:29:12 -0600

    Update chat_list_screen.dart

--- lib/screens/chat_list_screen.dart
+++ lib/screens/chat_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:project_servify/models/usuarios_model.dart'; // <--- IMPORTAR
+import 'package:project_servify/models/usuarios_model.dart'; 
 import 'package:project_servify/screens/chat_screen.dart';
 
 class ChatListScreen extends StatelessWidget {
